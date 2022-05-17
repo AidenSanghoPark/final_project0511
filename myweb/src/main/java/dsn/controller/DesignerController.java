42dbd4181120a2d3714b703f0eeceb2866c19c30
yunjulee <elige7205@naver.com>
2022-05-17 15:17:41 +0900

    윤주커밋
    답답해

---
+++ myweb/src/main/java/dsn/controller/DesignerController.java
@@ -0,0 +1,186 @@
+package dsn.controller;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import dsn.designer.model.DesignerDTO;
+import dsn.designer.model.DesignerService;
+import dsn.member.model.MemberDTO;
+import dsn.profile.model.ProfileDTO;
+
+import java.io.*;
+
+@Controller
+public class DesignerController {
+	
+	@Autowired
+	private DesignerService designerSevice;
+	
+	@Autowired ServletContext servletContext;
+	
+	@RequestMapping("portfolio.do")
+	public ModelAndView portfolio(@RequestParam(value = "cp", defaultValue = "1") int cp, HttpSession session) {
+		
+		int u_idx=(int)session.getAttribute("u_idx");
+		
+		int totalCnt=designerSevice.portfolioTotalCnt();
+		int listSize=5;
+		int pageSize=5;
+		String pageStr=dsn.page.PageModule.pageMake("portfolio.do", totalCnt, listSize, pageSize, cp);
+		
+		List lists=designerSevice.portfolio(cp, listSize, u_idx);
+		
+		int win=designerSevice.designerWin(u_idx);
+		ProfileDTO pdto=designerSevice.profileInfo(u_idx);
+		MemberDTO udto=designerSevice.userInfo(u_idx);
+		
+		ModelAndView mav=new ModelAndView();
+		
+		mav.addObject("lists", lists);
+		mav.addObject("pageStr", pageStr);
+		
+		mav.addObject("win", win);
+		mav.addObject("pdto", pdto);
+		mav.addObject("udto", udto);
+		mav.addObject("u_idx", u_idx);
+		
+		mav.setViewName("designer/portfolio");
+		return mav;
+	}
+	
+//	@RequestMapping("profile.do")
+//	public ModelAndView profile() {
+//		ModelAndView mav=new ModelAndView();
+//		mav.setViewName("designer/profile");
+//		return mav;
+//	}
+	
+	@RequestMapping(value = "profile.do", method = RequestMethod.GET)
+	public ModelAndView profileContent(HttpSession session) {
+		
+		//String path = servletContext.getRealPath("/");
+		//System.out.println(path); // Path test code
+		
+		int u_idx=(int)session.getAttribute("u_idx");
+		
+		int win=designerSevice.designerWin(u_idx);
+		ProfileDTO pdto=designerSevice.profileInfo(u_idx);
+		MemberDTO udto=designerSevice.userInfo(u_idx);
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("win", win);
+		mav.addObject("pdto", pdto);
+		mav.addObject("udto", udto);
+		mav.addObject("u_idx", u_idx);
+		mav.setViewName("designer/profile");
+		return mav;
+	}
+	
+	@RequestMapping(value = "profileInsert.do")
+	public ModelAndView profileInsert(MultipartHttpServletRequest request, ProfileDTO dto) {
+		String path=request.getSession().getServletContext().getRealPath("/profileimg/");
+		capyInto(path, dto.getUpload());
+		int result=designerSevice.profileInsert(dto);
+		String msg=result>0?"프로필 입력 완료":"프로필 입력 실패";
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg", msg);
+		mav.addObject("gopage", "profile.do");
+		mav.setViewName("designer/profileMsg");
+		return mav;
+	}
+	
+	@RequestMapping(value = "profileUpdate.do", method = RequestMethod.POST)
+	public ModelAndView profileUpdate(MultipartHttpServletRequest request, ProfileDTO dto) {
+		String path=request.getSession().getServletContext().getRealPath("/profileimg/");
+		capyInto(path, dto.getUpload());
+		dto.setP_img(dto.getUpload().getOriginalFilename());
+		int result=designerSevice.profileUpdate(dto);
+		
+		System.out.println(dto.getP_img());
+		
+		String msg=result>0?"프로필 수정 완료":"프로필 수정 실패";
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg", msg);
+		mav.addObject("gopage", "profile.do");
+		mav.setViewName("designer/profileMsg");
+		return mav;
+	}
+	
+	/**파일 복사 관련 메서드*/
+	public void capyInto(String path,MultipartFile upload) {
+		try {
+			byte bytes[]=upload.getBytes();
+			File f=new File(path+upload.getOriginalFilename());
+			FileOutputStream fos=new FileOutputStream(f);
+			fos.write(bytes);
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@RequestMapping("review.do")
+	public ModelAndView review(@RequestParam(value = "cp", defaultValue = "1") int cp, HttpSession session) {
+		
+		int u_idx=(int)session.getAttribute("u_idx");
+		
+		int totalCnt=designerSevice.reviewTotalCnt();
+		int listSize=5;
+		int pageSize=5;
+		String pageStr=dsn.page.PageModule.pageMake("review.do", totalCnt, listSize, pageSize, cp);
+		
+		int win=designerSevice.designerWin(u_idx);
+		ProfileDTO pdto=designerSevice.profileInfo(u_idx);
+		MemberDTO udto=designerSevice.userInfo(u_idx);
+		
+		List lists=designerSevice.review(cp, listSize, u_idx);
+		
+		ModelAndView mav=new ModelAndView();
+		
+		mav.addObject("lists", lists);
+		mav.addObject("pageStr", pageStr);
+		
+		mav.addObject("win", win);
+		mav.addObject("pdto", pdto);
+		mav.addObject("udto", udto);
+		mav.addObject("u_idx", u_idx);
+		
+		mav.setViewName("designer/review");
+		return mav;
+	}
+	
+	@RequestMapping(value = "designer.do", method = RequestMethod.GET)
+	public ModelAndView designer(@RequestParam(value = "cp", defaultValue = "1") int cp, DesignerDTO dto) {
+		
+		int totalCnt=designerSevice.designerTotalCnt();
+		int listSize=5;
+		int pageSize=5;
+		String pageStr=dsn.page.PageModule.pageMake("designer.do", totalCnt, listSize, pageSize, cp);
+		
+		int u_idx=dto.getU_idx();
+		List lists=designerSevice.designerList(cp, listSize, u_idx);
+		ProfileDTO pdto=designerSevice.profileInfo(u_idx);
+		List photos=designerSevice.designPhotos(u_idx);
+		System.out.println(photos);
+
+		ModelAndView mav=new ModelAndView();
+		
+		mav.addObject("lists", lists);
+		mav.addObject("pageStr", pageStr);
+		mav.addObject("pdto", pdto);
+		mav.addObject("photos", photos);
+
+		mav.setViewName("designer/designer");
+		return mav;
+	}
+}
---
+++ myweb/src/main/webapp/WEB-INF/views/designer/designer.jsp
@@ -0,0 +1,125 @@
+ <%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<style>
+.icons i {
+  color: #b5b3b3;
+  border: 1px solid #b5b3b3;
+  padding: 6px;
+  margin-left: 4px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.activity-done {
+  font-weight: 600;
+}
+
+.list-group li {
+  margin-bottom: 12px;
+}
+
+.list-group-item {
+}
+
+.list li {
+  list-style: none;
+  padding: 10px;
+  border: 1px solid #e3dada;
+  margin-top: 12px;
+  border-radius: 5px;
+  background: #fff;
+}
+
+.checkicon {
+  color: green;
+  font-size: 19px;
+}
+
+.date-time {
+  font-size: 12px;
+}
+
+.profile-image img {
+  margin-left: 1px;
+}
+.designer{
+	float: center;
+}
+.designerName{
+	float: right;
+}
+img {
+ 	max-width: 100%;
+ 	max-height: 100%;
+}
+.profileimg {
+	height: 100px;
+	 width: 100px;
+	 float: left;
+}
+.desingerNick{
+	float: right;
+	padding-top: 35px;
+}
+</style>
+</head>
+<body>
+<%@include file="/WEB-INF/views/header.jsp" %>
+<form name="designers">
+	<h2>&nbsp;&nbsp;&nbsp;디자이너</h2><br>
+	<br>
+	<div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-between align-items-center activity">
+                    <div><span class="activity-done"></span></div>
+                    <div class="icons"><input type="text" name="designerName"><input type="submit" value="검색" class="fa fa-search"></div>
+                </div>
+                <c:forEach var="dto" items="${lists}">
+                <div class="mt-3">
+                    <ul class="list list-inline">
+                        <li class="d-flex justify-content-between">
+                            <div class="d-flex flex-row align-items-center"><i class="fa fa-check-circle checkicon"></i>
+                                <div class="ml-2">
+                                	<div class="profileimg" style="padding:10px;"><img src="profileimg/${pdto.p_img}"></div>
+                                	<div class="desingerNick" style="padding-left: 10px; font-weight: bold;">${dto.u_nick}</div>
+                                    <div class="d-flex flex-row mt-1 text-black-50 date-time">
+                                    </div>
+                                    <div>
+                                    	<span class="ml-2" style="padding-left: 15px;">
+	                                    	<input type="button" value="포트폴리오" onclick="#">&nbsp;&nbsp;
+	                                    	<input type="button" value="이메일" onclick="#">
+                                    	</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-row align-items-center" style="width:400px;">
+                                <div class="d-flex flex-column mr-2" style="width:400px;">
+                                    <c:forEach var="photo" items="${photos}" begin="0" end="">
+		                                    <div class="profile-image">
+		                                    	<img src="img/${photo.d_img_1}" width="100">&nbsp;&nbsp;&nbsp;&nbsp;
+		                                    </div>
+                                    </c:forEach>
+                                </div>
+                                <i class="fa fa-ellipsis-h"></i>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                </c:forEach>
+                <div id="page">${pageStr}</div>
+            </div>
+        </div>
+    </div>
+</form>
+<%@include file="/WEB-INF/views/footer.jsp" %>
+</body>
+</html>
