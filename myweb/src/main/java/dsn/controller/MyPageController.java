7ae22d105d710464d5eb49ac4eda1e7bfefbaf8b
hanbee <zeukuez@gmail.com>
2022-05-18 18:57:50 +0900

    contest commit(디스패쳐 추가)

--- myweb/src/main/java/dsn/controller/MyPageController.java
+++ myweb/src/main/java/dsn/controller/MyPageController.java
@@ -1,9 +1,6 @@
 package dsn.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import dsn.member.model.MemberDTO;
 import dsn.mypage.model.MyPageDTO;
 import dsn.mypage.model.MyPageService;
 
@@ -22,32 +18,22 @@ public class MyPageController {
 	@Autowired
 	private MyPageService myPageService;
 	
+	
 	@RequestMapping("myPage.do")
-	public ModelAndView myPage(@RequestParam(value = "cp",defaultValue = "1") int cp,HttpSession session){
+	public ModelAndView myPage(@RequestParam(value = "cp",defaultValue = "1") int cp){
 		
-		int vo=(int)session.getAttribute("u_idx");	
-		ModelAndView mav=new ModelAndView();
-		if(vo==0) {
-			String msg="로그인 후 이용해주세요";
-			mav.addObject("msg", msg);
-			mav.addObject("gopage","index.do");
-			return mav;
-			
-		}else {
 		int totalCnt=myPageService.getTotalCnt();
 		int listSize=5;
 		int pageSize=5;
 		String pageStr=dsn.page.PageModule.pageMake("myPage.do", totalCnt, listSize, pageSize, cp);
-		List lists=myPageService.myPageList(cp, listSize, vo);
-		List userinfo=myPageService.userInfoFind(vo);
+		List lists=myPageService.myPageList(cp, listSize);
 		
-		mav.addObject("lists", lists);
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("lists",lists);
 		mav.addObject("pageStr", pageStr);
-		mav.addObject("u_idx", vo);
-		mav.addObject("userinfo", userinfo);
 		mav.setViewName("mypage/mypage");
 		return mav;
-		}
+				
 	}
 	@RequestMapping("accountConfig.do")
 	public ModelAndView accountConfig() {
@@ -91,22 +77,6 @@ public class MyPageController {
 	public ModelAndView profileConfig() {
 		ModelAndView mav=new ModelAndView();
 		mav.setViewName("mypage/profile");
-		return mav;
-	}
-	@RequestMapping("wallet.do")
-	public ModelAndView virtualWallet(@RequestParam(value = "cp",defaultValue = "1") int cp) {
-		int totalCnt=myPageService.getTotalCnt();
-		int blc=myPageService.getLastBalance();
-		int listSize=5;
-		int pageSize=5;
-		String pageStr=dsn.page.PageModule.pageMake("wallet.do", totalCnt, listSize, pageSize, cp);
-		List lists=myPageService.virtualWallet(cp, listSize);
-		
-		ModelAndView mav=new ModelAndView();
-		mav.addObject("lists",lists);
-		mav.addObject("pageStr", pageStr);
-		mav.addObject("blc", blc);
-		mav.setViewName("mypage/wallet");
 		return mav;
 	}
 }
