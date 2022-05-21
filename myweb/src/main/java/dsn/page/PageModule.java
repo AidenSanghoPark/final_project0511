c270cb142e67c2ab2547c1aedde9a4f61c9cac12
AidenSanghoPark <passort4@naver.com>
2022-05-21 20:32:43 +0900

    관리자 페이징 통합

--- myweb/src/main/java/dsn/page/PageModule.java
+++ myweb/src/main/java/dsn/page/PageModule.java
@@ -123,58 +123,6 @@ public class PageModule {
 			return sb.toString();
 		}
 	  
-	public static String pageMake(String pageName, int totalCnt, int listSize,int pageSize, int cp) {
-		
-		int totalPage = (totalCnt / listSize)+1;
-		if(totalCnt%listSize==0)totalPage--;
-
-		int userGroup = cp/pageSize;  
-		if(cp%pageSize==0)userGroup--; 
-
-		StringBuffer sb=new StringBuffer();
-		
-		sb.append("<nav aria-label='...'><ul class='pagination justify-content-center'>");
-		
-		
-		if(userGroup!=0){
-			sb.append("<li class='page-item'><a href='");
-			sb.append(pageName);
-			sb.append("?cp=");
-			int temp = (userGroup-1)*pageSize+pageSize;
-			sb.append(temp);
-			sb.append("'>&lt;&lt;</a>");
-			sb.append("</li>");
-		}
-		
-		
-		for(int i=(userGroup*pageSize+1);
-				i<=(userGroup*pageSize+pageSize);i++){
-			sb.append("<li class='page-item'><a href='");
-			sb.append(pageName);
-			sb.append("?cp=");
-			sb.append(i);
-			sb.append("' class='page-link'>");
-			sb.append(i);
-			sb.append("</a>&nbsp;&nbsp;");
-			
-			sb.append("</a></li>");
-	
-			if(i==totalPage)break;
-		}
-		
-		if(userGroup!=(totalPage/pageSize-(totalPage%pageSize==0?1:0))){
-			sb.append("<li class='page-item'><a href='");
-			sb.append(pageName);
-			sb.append("?cp=");
-			int temp=(userGroup+1)*pageSize+1;
-			sb.append(temp);
-			sb.append("'>&gt;&gt;</a>");
-		
-		}
-		
-		sb.append("</ul></nav>");
-		return sb.toString();
-	}
 
 	public static String searchPageMake(
 			String pageName,int totalCnt,int listSize,int pageSize,int cp,
